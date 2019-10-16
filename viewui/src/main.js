769bc2806acd207c9f330a92a0a49449ad49a4ce
申法宽 <shenfakuan@163.com>
2019-10-16 09:42:08 +0800

    add viewui

---
+++ viewui/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App';
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+Vue.use(ViewUI);
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+});
